73c64111ce866d978fc24f3870fe8275f9845d18
Nir Alfasi <alfasi@netflix.com>
2015-02-10 20:12:28 -0800

    second commit

---
+++ calculator.rb
@@ -0,0 +1,23 @@
+def is_number? expr
+  return false if expr.nil?
+  expr = "#{expr}"              # we need this condition in case the expr is a number
+  expr.match /^(\d+|\d+\.\d+)$/ # since match() is defined only for strings
+end
+ 
+def calc(expr)  
+  return expr.to_i if is_number? expr
+  expr.gsub!(" ","") # clean the string from whitespaces
+  # pay attention to the order: + and - should come before * and /
+  # can you figure out why ?
+  arr = expr.split /\+/
+  return arr.inject(0){|x,y| calc(x) + calc(y) } if arr.size > 1
+  arr = expr.split /\-/  
+  return arr.inject(0){|x,y| calc(x) - calc(y) } if arr.size > 1
+  arr = expr.split /\*/
+  return arr.inject(1){|x,y| calc(x) * calc(y) } if arr.size > 1
+  arr = expr.split /\//
+  return arr.inject   {|x,y| calc(x) / calc(y) } if arr.size > 1
+end
+ 
+puts calc("5+5* 6+4/2.0")
+#output 37
